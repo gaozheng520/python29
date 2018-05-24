4e400345acaa7901c5fb65912f7d1a3935abf06a
gaozheng <15383479200@189.cn>
2018-05-24 15:42:37 +0800

    moban

---
+++ hongdeshiyong.py
@@ -0,0 +1,8 @@
+from  flask import render_template,Flask
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template("hong.html")
+if __name__ == '__main__':
+    app.run(debug=True)
+
